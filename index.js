8789592266fed06672977288a0877c6b7303552f
nhat482 <tuphanminhnhat@gmail.com>
2022-04-08 16:57:02 +0700

    commit all

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const res = require("express/lib/response")
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/',(req,res)=>{
-    res.send('Hello Word')
-})
+
+app.set('view engine', 'pug');
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+    res.render('index',{
+        name:'Nhat tu'
+    })
+  })  
 //est
 app.get('/user',(req,res)=>{
     res.send('Minh Nhat')
