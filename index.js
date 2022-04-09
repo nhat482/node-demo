3a1db1f59e107191f58bdea694e68aa8941ecf2f
nhat482 <tuphanminhnhat@gmail.com>
2022-04-09 17:28:12 +0700

    search

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-const req = require("express/lib/request")
-const res = require("express/lib/response")
-
-const express = require('express')
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const app = express()
 const port = 3000
 
 app.set('view engine', 'pug');
 app.set('views', './views')
 
+//list all user
+var users = [
+    {name:"Minh Nhat", email: "nhat@gamil.com"},
+    {name:"Eden", email: "eden@gamil.com"}
+]
+
 app.get('/', (req, res) => {
     res.render('index',{
         name:'Nhat tu'
     })
   })  
-//est
-app.get('/user',(req,res)=>{
+//get users
+app.get('/users',function(req,res){
     res.render('users/list',{
-        users:[
-            {id:1, name:'Minh'},
-            {id:2, name: 'Nhat'}
-        ]
+        users:users
     })
 })
+
+app.get('/users/search',(req,res)=> {
+    var name_search = req.query.name
+    var result = users.filter((user)=>{
+        return user.name.toLowerCase().indexOf(name_search.toLowerCase())!==-1
+
+    })
+    res.render('users/list',{
+        users:result
+    })
+
+})
+
 app.listen(port,()=>{
     console.log('Example app listening on port '+port)
 })
