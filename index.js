afec472d7edf5b4914115ed1ef42f88477aa26cc
nhat482 <tuphanminhnhat@gmail.com>
2022-04-11 16:34:14 +0700

    new ruoting

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const req = require('express/lib/request');
 const res = require('express/lib/response');
 const app = express()
 const port = 3000
-
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.set('view engine', 'pug');
 app.set('views', './views')
 
 //list all user
 var users = [
-    {name:"Minh Nhat", email: "nhat@gamil.com"},
-    {name:"Eden", email: "eden@gamil.com"}
+    {id:1, name:"Minh Nhat", email: "nhat@gamil.com"},
+    {id:2, name:"Eden", email: "eden@gamil.com"}
 ]
 
 app.get('/', (req, res) => {
@@ -24,7 +25,7 @@ app.get('/users',function(req,res){
         users:users
     })
 })
-
+//search user
 app.get('/users/search',(req,res)=> {
     var name_search = req.query.name
     var result = users.filter((user)=>{
@@ -35,6 +36,28 @@ app.get('/users/search',(req,res)=> {
         users:result
     })
 
+})
+//get form create user
+app.get('/users/create',(req,res)=>{
+    res.render('users/create')
+})
+//create user
+app.post('/users/create',(req,res)=>{
+    console.log(req.body)
+    users.push(req.body)
+    res.redirect('/users')
+})
+//routing xem tung user
+app.get('/users/:id', (req, res) => {
+    // Tìm user phù hợp với params id
+	var user = users.find( (user) => {
+		return user.id == parseInt(req.params.id);
+	});
+    
+    // Render trang show, với một biến user được định nghĩa là user vừa tìm được
+	res.render('users/show', {
+    	user: user
+    })
 })
 
 app.listen(port,()=>{
